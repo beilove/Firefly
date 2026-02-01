5cf74674ccac90d90436c2dcc5cfe38cddefd664
beilove <70726513+beilove@users.noreply.github.com>
2026-02-01 17:16:58 +0000

    Deploying to pages from @ beilove/Firefly@9da770ac446e5a7234eeb5846874f0352f218e4e 🚀

--- _astro/KatexManager.astro_astro_type_script_index_0_lang.BxacUBQY.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/katex.BRd0jKnN.css"])))=>i.map(i=>d[i]);
-import{_ as t}from"./preload-helper.BlTxHScW.js";function e(){document.querySelector(".katex")&&t(()=>Promise.resolve({}),__vite__mapDeps([0]))}e();document.addEventListener("swup:content:replace",e);
